6ac8a36ede43cea67979e198ef78216365e1e580
David M. <david.mosquera.1@ulaval.ca>
2026-07-25 09:42:39 -0400

    delete useless starterkits

--- Python/bot.py
+++
@@ -1,8 +0,0 @@
-from client.message_protocol import *
-
-class Bot:
-    # EDIT THIS FOR YOUR OWN BOT TOKEN
-    TOKEN = "BOTA-abcd-1234-ABCD"
-
-    def get_next_action(self, state: GameState) -> ActionBase | None:
-        return None  # Placeholder for the bot's logic implementation
--- Python/main.py
+++
@@ -1,8 +0,0 @@
-from bot import Bot
-from bot_logic.bot_runner import BotRunner
-
-URL_REMOTE = "https://jg26.jdis.ca"
-
-if __name__ == "__main__":
-    print("Starting JDIS Bot Client...")
-    BotRunner.run(URL_REMOTE, Bot.TOKEN)
